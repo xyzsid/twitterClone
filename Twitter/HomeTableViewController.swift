eadcee9511d1c384756f837afddf5eb6f025534c
xyzsid <sid.dommeti@gmail.com>
2021-10-04 00:04:05 -0500

    added post functionality

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -20,6 +20,8 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 170
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -98,7 +100,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
-
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        
         return cell
     }
     
